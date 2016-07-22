644702cdf41cf905e1eb75c1e88f14593f1471ef
Andrew Leung <aleung@pivotal.io>
2016-07-22 15:14:04 +1000

    Add ability to edit a task [#125381731]
    Signed-off-by: Andrew Leung <aleung@pivotal.io>

--- app/src/main/java/io/pivotal/todolistandroid/adapter/TaskListAdapter.java
+++ app/src/main/java/io/pivotal/todolistandroid/adapter/TaskListAdapter.java
@@ -1,8 +1,11 @@
 package io.pivotal.todolistandroid.adapter;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.List;
@@ -25,9 +28,29 @@ public class TaskListAdapter extends RecyclerView.Adapter {
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        TaskViewHolder taskViewHolder = (TaskViewHolder) holder;
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+        final TaskViewHolder taskViewHolder = (TaskViewHolder) holder;
         taskViewHolder.textView.setText(tasks.get(position));
+        taskViewHolder.textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                View parent = View.inflate(v.getContext(), R.layout.view_editdialog, null);
+                final EditText child = (EditText)parent.findViewById(R.id.edit_task);
+                child.setText(taskViewHolder.textView.getText());
+
+                builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        tasks.set(position, child.getText().toString());
+                        notifyDataSetChanged();
+                    }
+                });
+
+                builder.setView(parent);
+                builder.create().show();
+            }
+        });
     }
 
     @Override
--- app/src/test/java/io/pivotal/todolistandroid/adapter/TaskListAdapterTest.java
+++ app/src/test/java/io/pivotal/todolistandroid/adapter/TaskListAdapterTest.java
@@ -1,5 +1,9 @@
 package io.pivotal.todolistandroid.adapter;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -8,11 +12,13 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowAlertDialog;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import io.pivotal.todolistandroid.BuildConfig;
+import io.pivotal.todolistandroid.R;
 
 import static org.assertj.android.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,6 +48,35 @@ public class TaskListAdapterTest {
         taskListAdapter.addTask("This is a New Task");
         assertThat(taskListAdapter.getItemCount()).isEqualTo(1);
         assertThat(taskListAdapter.getTasks().get(0)).isEqualTo("This is a New Task");
+    }
+
+    @Test
+    public void editTask_shouldDisplayDialog() {
+        TaskListAdapter taskListAdapter = new TaskListAdapter(Arrays.asList("Task 1", "Task 2"));
+        TaskListAdapter.TaskViewHolder viewHolder = (TaskListAdapter.TaskViewHolder) taskListAdapter.onCreateViewHolder(new LinearLayout(RuntimeEnvironment.application), 0);
+        taskListAdapter.onBindViewHolder(viewHolder, 0);
+
+        viewHolder.textView.performClick();
+
+        AlertDialog latestAlertDialog = ShadowAlertDialog.getLatestAlertDialog();
+        assertThat(latestAlertDialog).isNotNull();
+
+        EditText editTask = (EditText) latestAlertDialog.findViewById(R.id.edit_task);
+        assertThat(editTask).hasText("Task 1");
+
+        Button saveButton = latestAlertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+
+        assertThat(saveButton).hasText("Save");
+
+        String newTask = "Say hello to people";
+
+        editTask.setText(newTask);
+        saveButton.performClick();
+
+        // Mimic notifyDataSetChanged by rebinding
+        taskListAdapter.onBindViewHolder(viewHolder, 0);
+        assertThat(viewHolder.textView).hasText(newTask);
+        assertThat(taskListAdapter.getTasks().get(0)).isEqualTo(newTask);
     }
 
 }
